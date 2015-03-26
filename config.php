685d5b2796d2a40c28c8be645bc4a40d9eac4ac2
bengaddie <benjamin.gaddie@gmail.com>
2015-03-26 12:57:24 -0400

    Added the rest of the initial files.

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$currency = '$'; //Currency sumbol or code
+
+$db_username = 'btga224';
+$db_password = 'u0705355';
+$db_name = 'test';
+$db_host = 'mastelottoplan.backups.uky.edu';
+$mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
+?>
